4763f23c0d7bd75423fa3ae4bd9e4400430601cf
WilliamFourcade <145121886+WilliamFourcade@users.noreply.github.com>
2023-11-10 15:50:57 +0100

    Add files via upload

---
+++ TP4 javascript/tp4.js
@@ -0,0 +1,83 @@
+function seconnecter(){
+    var login=prompt("Donner votre nom d'utilisateur")
+    var password= prompt("Entrer votre mot de passe")
+    if(login=="admin" && password=="admin"){
+        document.write('Bienvenu:' +login)
+    }
+    else{
+        alert("Accès refusé")
+    }
+}
+function seconnecter2(){
+    window.location.href="identification.html"
+}
+function login(){
+    var login=document.getElementById("t1").value;
+    var password=document.getElementById("h1").value;
+    if(login=="admin" && password=="admin"){
+        window.location.href="yes.html"
+    }
+    else{
+        window.location.href="no.html"
+    }
+}
+function affichagetab(){
+    document.write('<table border=2px width=30%');
+    for(let i=0;i<=5;i++){
+        document.write('<tr><td>*</td><td>*</td><td>*</td></tr>');
+    }
+    document.write('</table>');
+}
+function affichagetab2(){
+    var k=prompt("Nombre de ligne")
+    document.write('<table border=2px width=30%');
+    for(let i=0;i<=k;i++){
+        document.write('<tr><td>*</td><td>*</td><td>*</td></tr>');
+    }
+    document.write('</table>');
+}
+function swich(){
+    var c= prompt("Choisir une couleur")
+   switch(c){
+    case "rouge":document.body.style.background="red";
+    break;
+    case "ROUGE":document.body.style.background="red";
+    break;
+    case "jaune":document.body.style.background="yellow";
+    break;
+    case "JAUNE":document.body.style.background="yellow";
+    break;
+    case "vert":document.body.style.background="green";
+    break;
+    case "VERT":document.body.style.background="green";
+    break;
+    default:alert("couleur non définie")
+   }
+}
+function cdc(){
+    var chaine = prompt("Donnez un mot")
+    document.write(chaine.toUpperCase()+"<br>");
+    document.write(chaine.toLowerCase()+"<br>");
+    document.write("la chaine contient"+chaine.length+"caractère"+"<br>")
+    document.write("la première lettre est"+chaine.substring(0.1)+"<br>")    
+}
+function seconnecter3(){
+    var i = 0;
+    do{
+    var login=prompt("Donner votre nom d'utilisateur");
+    var password= prompt("Entrer votre mot de passe");
+    if(login=="admin" && password=="admin"){
+        document.write('Bienvenu:' +login);
+        break;
+    }
+    else{
+        alert("Accès refusé");
+        i+=1;
+    }}
+    while(i<3);
+    alert("Délai dépassé");
+    
+
+
+
+}
